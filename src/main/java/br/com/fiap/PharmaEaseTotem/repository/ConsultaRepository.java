9f18f1e5d74befd83ff31bd938478c52378ffb93
WINDOWS <arthuryamamoto0@gmail.com>
2024-04-15 08:44:06 -0300

    feat: delete consulta class

--- src/main/java/br/com/fiap/PharmaEaseTotem/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.PharmaEaseTotem.repository;
-
-import br.com.fiap.PharmaEaseTotem.model.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-    // Métodos personalizados, se necessário
-}
-
